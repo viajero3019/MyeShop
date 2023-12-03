ac2ef052b9de2d693a494ec4b77ea283f79ea537
Santiago Castañeda <santyago_999@hotmail.com>
2023-12-02 19:03:29 -0500

    Adding Cookie Authentication and Logout Page

---
+++ src/Infrastructure/Identity/AppIdentityDbContextSeed.cs
@@ -0,0 +1,34 @@
+using System.Net;
+using Microsoft.AspNetCore.Identity;
+
+namespace MyeShop.Infrastructure.Identity;
+
+public class AppIdentityDbContextSeed
+{
+    public const string ADMINISTRATORS = "Administrators";
+    public const string DEFAULT_PASSWORD = "Pass@word1";
+
+    public static async Task SeedAsync(AppIdentityDbContext appIdentityDbContext, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+    {
+        Console.WriteLine("--> Seeding Users");
+
+        await roleManager.CreateAsync(new IdentityRole(ADMINISTRATORS));
+
+        var defaultUser = new ApplicationUser { UserName = "test", Email = "test@test.com"};
+       
+        await userManager.CreateAsync(defaultUser, DEFAULT_PASSWORD);
+
+        string adminUserName = "admin@test.com";
+
+        var adminUSer = new ApplicationUser { UserName = adminUserName, Email = adminUserName};
+
+        await userManager.CreateAsync(adminUSer, DEFAULT_PASSWORD);
+
+        adminUSer = await userManager.FindByNameAsync(adminUserName);
+
+        if(adminUSer != null) 
+        {
+            await userManager.AddToRoleAsync(adminUSer, ADMINISTRATORS);
+        }
+    }
+}
--- src/WebApp/Program.cs
+++ src/WebApp/Program.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.DependencyInjection;
 using MyeShop.Infrastructure.Identity;
+using MyeShop.WebApp.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Logging.AddConsole();
@@ -10,6 +12,16 @@ if (builder.Environment.IsDevelopment())
 {
     MyeShop.Infrastructure.Dependencies.ConfigureServices(builder.Configuration, builder.Services);
 }
+
+builder.Services.AddCookieSettings();
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options => 
+    {
+        options.Cookie.HttpOnly = true;
+        options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+        options.Cookie.SameSite = SameSiteMode.Lax;
+    });
 
 builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
     .AddDefaultUI()
@@ -20,7 +32,15 @@ builder.Services.AddControllersWithViews();
 
 builder.Services.AddRazorPages();
 
+Console.WriteLine("--> End Build Services");
+
+
 var app = builder.Build();
+
+app.Logger.LogInformation("--> App created...");
+
+
+
 
 app.UseStaticFiles();
 
@@ -30,7 +50,25 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+using (var scope = app.Services.CreateScope())
+{
+    var scopeProvider = scope.ServiceProvider;
+    try
+    {
+        app.Logger.LogInformation("Seeding the DB.");
 
-// app.MapGet("/other", () => "Hello, other");
+        var userManager = scopeProvider.GetRequiredService<UserManager<ApplicationUser>>();
+        var roleManager = scopeProvider.GetRequiredService<RoleManager<IdentityRole>>();
+        var IdentityContext = scopeProvider.GetRequiredService<AppIdentityDbContext>();
+        await AppIdentityDbContextSeed.SeedAsync(IdentityContext, userManager, roleManager);
+    }
+    catch (System.Exception ex)
+    {
+        app.Logger.LogError(ex, "An error occurred seeding the DB.");
+        throw;
+    }
+}
+
+app.Logger.LogInformation("LAUNCHING");
 
 app.Run();
